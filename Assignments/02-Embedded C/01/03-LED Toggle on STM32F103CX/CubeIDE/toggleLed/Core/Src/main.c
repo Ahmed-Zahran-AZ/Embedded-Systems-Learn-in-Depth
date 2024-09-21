e0d57c3db79d6e9096cf82a6ed6501442dfdd271
Ahmed Zahran <ahmed.zahran.aaz@gmail.com>
2024-09-21 17:48:09 +0300

    Embedded C Assignment 01 - LED Toggle

--- Assignments/02-Embedded C/01/03-LED Toggle on STM32F103CX/CubeIDE/toggleLed/Core/Src/main.c
+++ Assignments/02-Embedded C/01/03-LED Toggle on STM32F103CX/CubeIDE/toggleLed/Core/Src/main.c
@@ -7,11 +7,41 @@
 typedef union {
 	vuint32_t all_fields;
 	struct{
-		vuint32_t reserved:12;
+		vuint32_t pin0:1;
+		vuint32_t pin1:1;
+		vuint32_t pin2:1;
+		vuint32_t pin3:1;
+		vuint32_t pin4:1;
+		vuint32_t pin5:1;
+		vuint32_t pin6:1;
+		vuint32_t pin7:1;
+		vuint32_t pin8:1;
+		vuint32_t pin9:1;
+		vuint32_t pin10:1;
+		vuint32_t pin11:1;
+		vuint32_t pin12:1;
 		vuint32_t pin13:1;
-	}pin;
+		vuint32_t pin14:1;
+		vuint32_t pin15:1;
+		vuint32_t pin16:1;
+		vuint32_t pin17:1;
+		vuint32_t pin18:1;
+		vuint32_t pin19:1;
+		vuint32_t pin20:1;
+		vuint32_t pin21:1;
+		vuint32_t pin22:1;
+		vuint32_t pin23:1;
+		vuint32_t pin24:1;
+		vuint32_t pin25:1;
+		vuint32_t pin26:1;
+		vuint32_t pin27:1;
+		vuint32_t pin28:1;
+		vuint32_t pin29:1;
+		vuint32_t pin30:1;
+		vuint32_t pin31:1;
+	};
 
-}R_ODR_t;
+}reg;
 
 
 #define RCC_APB2ENR *((vuint32_t*) (RCC_BASE+0x18))
@@ -19,7 +49,8 @@ typedef union {
 #define GPIOA_ODR	*((vuint32_t*) (PORTA_BASE+0x0C))
 
 
-volatile R_ODR_t* R_ODR = (volatile R_ODR_t *)(PORTA_BASE+0X0C);
+
+#define ODR (*(volatile  reg*)(PORTA_BASE+0x0C))
 
 void main(void){
 
@@ -28,9 +59,9 @@ GPIOA_CRH 	&= 0xff0fffff;
 GPIOA_CRH 	|= 0x00200000;
 
 while (1){
-for (int i =0; i<5000; i++);
-R_ODR->pin.pin13 = 1;
-for (int i =0; i<=5000; i++);
-R_ODR->pin.pin13 = 0;
+for (int i =0; i<100000; i++);
+ODR.pin13=0;
+for (int i =0; i<=100000; i++);
+ODR.pin13=1;
 }
 }
